b5c028877b63bf260789e105f4583b16d348dc73
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-15 12:31:44 +0200

    testing with sign out

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,17 @@
 
 console.log('app.js is alive!')
 
-// // Example 1: Use method-specific function
-// var request = gapi.client.drive.about.get({ 'fields': 'user' });
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
-// // Execute the API request.
-// request.execute(function(response) {
-//     console.log(response);
-// });
-
-
-// // Example 2: Use gapi.client.request(args) function
-// var request = gapi.client.request({
-//     'method': 'GET',
-//     'path': '/drive/v3/about',
-//     'params': { 'fields': 'user' }
-// });
-// // Execute the API request.
-// request.execute(function(response) {
-//     console.log(response);
-// });
-
-// gapi.load('auth2', function() {
-//     gapi.auth2.init();
-// });
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+        console.log('User signed out.');
+    });
+}

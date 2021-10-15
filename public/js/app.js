4fd8c8cf3bc5b0535cba9f25f6f588495ace7e7b
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-15 12:23:32 +0200

    more testing

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,26 @@
 
 console.log('app.js is alive!')
 
-// // Example 1: Use method-specific function
-// var request = gapi.client.drive.about.get({ 'fields': 'user' });
+// Example 1: Use method-specific function
+var request = gapi.client.drive.about.get({ 'fields': 'user' });
 
-// // Execute the API request.
-// request.execute(function(response) {
-//     console.log(response);
-// });
+// Execute the API request.
+request.execute(function(response) {
+    console.log(response);
+});
 
 
-// // Example 2: Use gapi.client.request(args) function
-// var request = gapi.client.request({
-//     'method': 'GET',
-//     'path': '/drive/v3/about',
-//     'params': { 'fields': 'user' }
-// });
-// // Execute the API request.
-// request.execute(function(response) {
-//     console.log(response);
-// });
+// Example 2: Use gapi.client.request(args) function
+var request = gapi.client.request({
+    'method': 'GET',
+    'path': '/drive/v3/about',
+    'params': { 'fields': 'user' }
+});
+// Execute the API request.
+request.execute(function(response) {
+    console.log(response);
+});
+
+gapi.load('auth2', function() {
+    gapi.auth2.init();
+})

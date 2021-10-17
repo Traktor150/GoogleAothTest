38a277fe3cc15c5f9b1ce1c8515a360f4ae8d1bc
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-17 21:49:19 +0200

    comments

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,22 @@
 console.log('app.js is alive!')
 
 function onSignIn(googleUser) {
+    // This function has to do with the old way button (should not be used anymore)
+    // The function gets information about the user on sign in
+
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
     if (window.location.href == 'http://localhost:8042/loggedIn') {
         console.log('Signed in')
     } else {
         location.href = 'http://localhost:8042/loggedIn';
     }
+    // This if else is what redirects between pages, there may be another way to do this
+    // But not important since this is the old way and should not be used anymore
 }
 
 function signOut() {

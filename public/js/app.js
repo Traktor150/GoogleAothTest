567030726aa8426692f84baaaaee3fb840f716aa
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-19 21:10:37 +0200

    trimmed the comments

--- public/js/app.js
+++ public/js/app.js
@@ -8,35 +8,18 @@ console.log('app.js is alive!')
 
 function handleCredentialResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
-    // "credential" is a coded id token that has the users information
-    // Needs to be decoded to be useful
-    // JWT stands for JSON Web Token
-    // Link to documentation
-    // https://developers.google.com/identity/gsi/web/guides/handle-credential-responses-js-functions
 
     const responsePayload = decodeJwtResponse(response.credential);
-    // decodeJwtResponse() is a custom function defined by you
-    // to decode the credential response
-    // https://jwt.io/
-    // This website can be used to decode the token which in this case is "response.credential"
-    // Unsure of how it can be used in programming
 
+    // responePayload contains the information from the decoded JWT
     console.log("ID: " + responsePayload.sub);
     console.log('Full Name: ' + responsePayload.name);
     console.log('Given Name: ' + responsePayload.given_name);
     console.log('Family Name: ' + responsePayload.family_name);
     console.log("Image URL: " + responsePayload.picture);
     console.log("Email: " + responsePayload.email);
-    // This is what shows user information
-    // responePayload conatains the information from the decoded JWT
 }
 window.onload = function() {
-    // Currently the code inside google.accounts.id.initialize does nothing,
-    // although may be used to redirect when using popupmode, unsure of how
-    // Link to documentation
-    // Link to documentation
-    // https://developers.google.com/identity/gsi/web/reference/js-reference#google.accounts.id.initialize
-
     google.accounts.id.initialize({
         client_id: "732451709774-58h1h3tbgr6c6c6ouce7lot3vh6d96u8.apps.googleusercontent.com",
         callback: handleCredentialResponse,
@@ -44,9 +27,7 @@ window.onload = function() {
 }
 
 function decodeJwtResponse(token) {
-    // This is what decides the JWT
-    // There are probably several ways to do this
-    // Got this from https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+    // Decodes JWT
 
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
